8f8764b0c6a89a6f972d3f75dc3cb3a6e083a938
Emad Saber Hamed <omda.sharkawy@gmail.com>
2018-06-13 23:20:53 +0200

    Tasks queue

---
+++ Tests/ESH.Log.Parser.Engine.Tests/Moqs/ParserMoqs.cs
@@ -0,0 +1,25 @@
+﻿using ESH.Log.Parser.Engine.Services.Parser.Support;
+using ESH.Log.Parser.Engine.Services.Reader.Support;
+using ESH.Log.Parser.Engine.Services.Support.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.Log.Parser.Engine.Tests.Moqs
+{
+    public static class ParserMoqs
+    {
+        public static ParserObject PO_SingleLineMessages => new ParserObject()
+        {
+            Format = null,
+            PlainLines = new List<PlainLine>() {
+                new PlainLine("" ) ,
+                new PlainLine("") ,
+                new PlainLine("")
+            }
+        };
+
+    }
+}
---
+++ Tests/ESH.Log.Parser.Engine.Tests/Services/Parser/ParserTests.cs
@@ -0,0 +1,23 @@
+﻿using ESH.Log.Parser.Engine.Services;
+using ESH.Log.Parser.Engine.Services.Parser.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.Log.Parser.Engine.Tests.Services.Parser
+{
+    [TestClass]
+    public class ParserTests
+    {
+        LogParser logParser = new LogParser();
+
+        [TestMethod]
+        public void Test_SingleLineMessage()
+        {
+            logParser.Target = 
+        }
+    }
+}
